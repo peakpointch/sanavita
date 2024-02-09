fa32fa7ea9ab16ea90576072606565cff454ceb9
lukas-peakpoint <graflukas218@gmail.com>
2024-02-10 00:49:50 +0100

    adding vimeo.js

--- builder.js
+++ builder.js
@@ -1,10 +1,10 @@
 require("esbuild")
     .build({
-        entryPoints: ["js-library/vimeo-component.ts"],
+        entryPoints: ["js-library/vimeo.ts"],
         bundle: true,
         minify: true,
         sourcemap: true,
-        outfile: "dist/vimeo-component.js",
+        outfile: "dist/vimeo.js",
         format: "esm",
     })
     .catch(() => process.exit(1));
--- dist/vimeo.js
+++ dist/vimeo.js
@@ -1,26 +1,2 @@
-(() => {
-  // js-library/vimeo.js
-  $(document).ready(function() {
-    var close_btn = $(".iphone_close-button");
-    close_btn.click(function() {
-      var sibling_video = $(this).siblings(".iphone_video").find("iframe")[0];
-      var sibling_src = $(sibling_video).attr("src");
-      if (sibling_video && sibling_src.includes("vimeo.com")) {
-        var sibling_player = new Vimeo.Player(sibling_video);
-        sibling_player.pause();
-      }
-      var video_play_sibling = $(this).siblings(".iphone_video-play").find("iframe[src*='https://player.vimeo.com/video/']")[0];
-      if (video_play_sibling) {
-        var video_play_player = new Vimeo.Player(video_play_sibling);
-        video_play_player.pause();
-      }
-      $("iframe[src*='?background=1']").each(function() {
-        var background_src = $(this).attr("src");
-        if (background_src.includes("vimeo.com")) {
-          var background_player = new Vimeo.Player(this);
-          background_player.play();
-        }
-      });
-    });
-  });
-})();
+$(document).ready(function(){var a=$(".iphone_close-button");a.click(function(){var i=$(this).siblings(".iphone_video").find("iframe")[0],n=$(i).attr("src");if(i&&n.includes("vimeo.com")){var o=new Vimeo.Player(i);o.pause()}var e=$(this).siblings(".iphone_video-play").find("iframe[src*='https://player.vimeo.com/video/']")[0];if(e){var r=new Vimeo.Player(e);r.pause()}$("iframe[src*='?background=1']").each(function(){var s=$(this).attr("src");if(s.includes("vimeo.com")){var c=new Vimeo.Player(this);c.play()}})})});
+//# sourceMappingURL=vimeo.js.map
