e4d7b0aa6bcff2ee50b0157b4628418d1e78a903
Lukas Graf <graflukas218@gmail.com>
2024-05-24 17:39:12 +0200

    infobanner: try dynamic infobanner

--- js-library/banner.js
+++ js-library/banner.js
@@ -1,13 +1,35 @@
 // Speziellen Infobanner in die Navbar verschieben
 
-const target = document.querySelector('[pp-type="infobanner"]');
-const elements = document.querySelectorAll('[pp-type="banner-element"]');
+const bannerType = {
+    lounge: "/limmathof-lounge",
+    thaiGarden: "/thai-garden"
+};
 
-if (target && elements.length > 0) {
+const nav = document.querySelector('[pp-type="nav-wrapper]');
+const bannerWrapper = nav.querySelector('[pp-type"infobanner-wrapper"]');
+const originalBanner = bannerWrapper.querySelector('[pp-type="nav-banner-list"]');
+// const elements = bannerWrapper.querySelectorAll('[pp-type="banner-list"]');
+
+const allBanners = bannerWrapper.querySelectorAll('[banner-type]');
+
+const path = window.location.pathname;
+
+allBanners.forEach(banner => {
+    let currentBannerType = banner.getAttribute('banner-type');
+    if (path === bannerType[currentBannerType]) {
+        banner.classList.toggle('show');
+    } else {
+        banner.classList.toggle('hide');
+    }
+});
+
+/*
+if (nav && elements.length > 0) {
     elements.forEach(el => {
-        target.insertAdjacentElement('afterend', el);
+        nav.insertBefore(el, nav.firstChild);
     });
-    target.remove();
+    originalBanner.remove();
 } else {
     console.error('Infobanner nicht gefunden');
 }
+*/
