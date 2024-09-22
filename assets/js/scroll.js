9bd76a4f74201ca92ac61953c565a003d93d6259
Lukas Graf <graflukas218@gmail.com>
2024-09-22 20:42:09 +0200

    update scroll.js

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,12 +1,12 @@
-function scrollToSection(id, offset=0) {
+function scrollToSection(id, offset = 0) {
   console.log("SCROLLING");
-  
-  setTimeout(() => {    
+
+  setTimeout(() => {
     const section = document.getElementById(id);
     if (section) {
       const elementPosition = section.getBoundingClientRect().top;
       const offsetPosition = elementPosition + window.pageYOffset - offset;
-  
+
       window.scrollTo({
         top: offsetPosition,
         behavior: 'smooth'
@@ -20,13 +20,20 @@ function scrollToSection(id, offset=0) {
 document.addEventListener("DOMContentLoaded", () => {
   const allScrollLinks = document.querySelectorAll('a[href^="#"], [scroll-to]');
 
+  const globalScrollLinks = documen.querySelectorAll('a[href*="#"]');
+  let globalFiltered = Array.from(globalScrollLinks).filter(link => link.href.includes(location.pathname));
+  globalFiltered.forEach(link => {
+    const url = new URL(link.href); // Create a URL object from the link's href
+    link.href = url.hash; // Set the href to only the hash (e.g., "#section")
+  });
+
   allScrollLinks.forEach(link => {
     link.addEventListener('click', (event) => {
       event.preventDefault();
       // var scrollId = link.getAttribute('scroll-to');
       const scrollId = link.getAttribute('href').slice(1) || link.getAttribute('scroll-to');
       console.log(scrollId);
-      
+
       const offset = link.getAttribute('scroll-offset') || 88;
       scrollToSection(scrollId, offset);
     });
