19b4b7e312ea4b01f9849f320d8a489df38ec578
Lukas Graf <graflukas218@gmail.com>
2024-10-09 13:00:52 +0200

    update sozjobs for pagination

--- assets/js/sozjobs.js
+++ assets/js/sozjobs.js
@@ -14,66 +14,89 @@
   const jobEmptyState = jobComponent.querySelector('[data-job-element="empty-state"]');
   const jobCardTemplate = jobComponent.querySelector('[data-job-element="template"]');
 
-  window.addEventListener('jobDataReady', () => {
-    console.log('JOB DATA READY');
-    jobEmptyState.remove();
+  let displayedJobs = 3; // Start with the first 3 jobs
 
+  window.addEventListener('jobDataReady', () => {
     const jobs = window.jobData; // Get all jobs fetched by api
     const categories = window.categoryData;
     const contractTypes = window.contractTypesData;
 
-    console.log(jobData);
+    console.log('JOB DATA READY', jobs);
 
-    jobs.forEach(job => {
-      const jobCard = jobCardTemplate.cloneNode(true);
-      jobCard.classList.remove('hide');
-      jobCard.classList.remove('is-template');
-      jobCard.style.display = "flex";
-      const props = ['title', 'accessionPer', 'rate', 'categoryNames', 'contractTypeName'];
+    if (!jobs.length) {
+      jobList.remove();
+      return
+    }
+    jobEmptyState.remove();
 
-      // Determine rate
-      if (job.isparttime) {
-        job.rate = `${job.parttimefrom}-${job.parttimeto}%`;
-      } else {
-        job.rate = `Vollzeit 100%`;
-      }
+    const displayJobs = (start, count) => {
+      const jobsToShow = jobs.slice(start, start + count);
+      jobsToShow.forEach(job => {
+        const jobCard = jobCardTemplate.cloneNode(true);
+        jobCard.classList.remove('hide');
+        jobCard.classList.remove('is-template');
+        jobCard.style.display = "flex";
+        const props = ['title', 'accessionPer', 'rate', 'categoryNames', 'contractTypeName'];
 
-      // Determine category names
-      job.categorynames = job.categories
-        .map(category => {
-          console.log('CATEGORY NAME:', category.name);
-          return category ? category.name : '';
-        })
-        .filter(name => name !== null)
-        .join(', ');
+        // Determine rate
+        if (job.isparttime) {
+          job.rate = `${job.parttimefrom}-${job.parttimeto}%`;
+        } else {
+          job.rate = `Vollzeit 100%`;
+        }
 
-      job.contracttypename = contractTypes.find(type => type.key === job.contracttype)?.value
+        // Determine category names
+        job.categorynames = job.categories
+          .map(category => category ? category.name : '')
+          .filter(name => name !== null)
+          .join(', ');
 
-      props.forEach(prop => {
-        const attr = `[data-job-${toKebabCase(prop)}]:not(a)`;
-        const el = jobCard.querySelector(attr);
-        // console.log(el);
-        el.innerText = job[prop.toLowerCase()];
-        el.dataset[toJobDataset(prop)] = job[prop.toLowerCase()] || "init";
+        job.contracttypename = contractTypes.find(type => type.key === job.contracttype)?.value;
 
-        if (prop === 'title') {
-          el.dataset.formSelectOptionValue = job[prop.toLowerCase()];
+        props.forEach(prop => {
+          const attr = `[data-job-${toKebabCase(prop)}]:not(a)`;
+          const el = jobCard.querySelector(attr);
+          el.innerText = job[prop.toLowerCase()];
+          el.dataset[toJobDataset(prop)] = job[prop.toLowerCase()] || "init";
+
+          if (prop === 'title') {
+            el.dataset.formSelectOptionValue = job[prop.toLowerCase()];
+          }
+        });
+
+        const cardLink = jobCard.querySelector('a');
+        cardLink.href = `/jobs/job?id=${job.identitynumber}`;
+        cardLink.target = "";
+
+        jobList.appendChild(jobCard);
+        console.log(job);
+      });
+    };
+
+    // Initial display of jobs
+    displayJobs(0, displayedJobs);
+
+    // Load More Button
+    if (jobs.length > displayedJobs) {
+      const loadButton = jobComponent.querySelector('[data-job-element="pagination"]');
+      loadButton.classList.remove('hide');
+
+      loadButton.addEventListener('click', () => {
+        displayedJobs += 3; // Increase the count of displayed jobs
+        displayJobs(displayedJobs - 3, 3); // Display the next set of jobs
+
+        // Hide the button if all jobs are displayed
+        if (displayedJobs >= jobs.length) {
+          loadButton.classList.add('hide');
         }
       });
-
-      const cardLink = jobCard.querySelector('a');
-      cardLink.href = `/jobs/job?id=${job.identitynumber}`;
-      cardLink.target = ""
-
-      jobList.appendChild(jobCard);
-      console.log(job);
-    });
+    }
 
     const cmsFormSelectReady = new Event('cmsFormSelectReady');
     jobList.dispatchEvent(cmsFormSelectReady);
-  })
+  });
 
   window.addEventListener('jobDataEmpty', () => {
     jobList.remove();
-  })
+  });
 })();
