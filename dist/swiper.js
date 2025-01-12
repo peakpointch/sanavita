b6538e7fb19e242a4cff7b8a32178726b3ae9845
Lukas Graf <graflukas218@gmail.com>
2025-01-12 12:17:44 +0100

    Ensure DOMContentLoaded, prepare for abstraction of swiperOptions

--- dist/swiper.js
+++ dist/swiper.js
@@ -9651,11 +9651,10 @@
   Swiper.use(modules);
 
   // src/js/swiper.js
-  function skipEmptySwiper(swiperElement) {
+  function swiperEmpty(swiperElement) {
     const slides = swiperElement.querySelectorAll(".swiper-slide");
     if (!slides.length > 0) {
       console.warn(`Swiper "${swiperElement.getAttribute("swiper-component")}": Skip empty component.`);
-      swiperElement.classList.add("hide");
       return true;
     }
     return false;
@@ -9680,91 +9679,151 @@
       disableOnInteraction: true
     };
   }
-  document.querySelectorAll(".w-slide:empty").forEach((e) => e.remove());
-  var webflowSwipers = document.querySelectorAll(
-    '[swiper-component]:not([swiper-component="default"])'
-  );
-  webflowSwipers.forEach((swiperElement) => {
-    if (skipEmptySwiper(swiperElement))
-      return;
-    swiperElement.classList.remove("initial-hide");
-    const swiperId = swiperElement.getAttribute("swiper-component");
-    const swiperMode = swiperElement.dataset.swiperMode;
-    const navigationPrefix = setNavigationPrefix(swiperId, swiperMode);
-    const dataNav = (swiperElement.dataset.swiperNav || ".swiper-button").toString();
-    const prevEl = `${navigationPrefix}${dataNav}:not(.next)`;
-    const nextEl = `${navigationPrefix}${dataNav}.next`;
-    const autoHeight = JSON.parse(
-      swiperElement.dataset.swiperAutoHeight || "false"
+  function initWebflowSwipers() {
+    document.querySelectorAll(".w-slide:empty").forEach((e) => e.remove());
+    const webflowSwipers = document.querySelectorAll(
+      '[swiper-component]:not([swiper-component="default"])'
     );
-    const slidesPerView = parseSlidesPerView(swiperElement.dataset.swiperSlidesPerView);
-    const spaceBetween = parseFloat(swiperElement.dataset.swiperSpace || 8);
-    const centeredSlides = JSON.parse(
-      swiperElement.dataset.swiperCenteredSlides || "false"
-    );
-    const loop2 = JSON.parse(swiperElement.dataset.swiperLoop || "true");
-    const allowTouchMove = JSON.parse(
-      swiperElement.dataset.swiperTouchMove || "true"
-    );
-    const autoplayBoolean = JSON.parse(swiperElement.dataset.swiperAutoplay || "true");
-    const autoplayDelay = parseFloat(swiperElement.dataset.swiperAutoplayDelay || 4e3);
-    const autoplayOptions = setupAutoplay(autoplayBoolean, autoplayDelay);
-    const speed = parseFloat(swiperElement.dataset.swiperSpeed || 400);
-    const swiper = new Swiper(swiperElement, {
-      navigation: {
-        prevEl,
-        nextEl
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true
-      },
-      autoplay: autoplayOptions,
-      allowTouchMove,
-      centeredSlides,
-      effect: "slide",
-      speed,
-      autoHeight,
-      spaceBetween,
-      loop: loop2,
-      slidesPerView,
-      debugger: true
-    });
-    swiper.autoplay.stop();
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          swiper.autoplay.start();
-        } else {
-          swiper.autoplay.stop();
+    webflowSwipers.forEach((swiperElement) => {
+      swiperElement.classList.remove("initial-hide");
+      const attributes = [
+        {
+          name: "swiper-component",
+          type: "component"
+        },
+        {
+          name: "data-swiper-mode",
+          type: "text"
+        },
+        {
+          name: "data-swiper-nav",
+          type: "text",
+          default: ".swiper-button"
+        },
+        {
+          name: "data-swiper-auto-height",
+          type: "boolean",
+          default: false
+        },
+        {
+          name: "data-swiper-slides-per-view",
+          type: "floatOrAuto"
+        },
+        {
+          name: "data-swiper-space",
+          type: "float",
+          default: 8
+        },
+        {
+          name: "data-swiper-centered-slides",
+          type: "boolean",
+          default: false
+        },
+        {
+          name: "data-swiper-loop",
+          type: "boolean",
+          default: true
+        },
+        {
+          name: "data-swiper-touch-move",
+          type: "boolean",
+          default: true
+        },
+        {
+          name: "data-swiper-autoplay",
+          type: "boolean",
+          default: true
+        },
+        {
+          name: "data-swiper-autoplay-delay",
+          type: "float",
+          default: 4e3
+        },
+        {
+          name: "data-swiper-speed",
+          type: "float",
+          default: 400
         }
+      ];
+      const swiperId = swiperElement.getAttribute("swiper-component");
+      const swiperMode = swiperElement.dataset.swiperMode;
+      const dataNav = (swiperElement.dataset.swiperNav || ".swiper-button").toString();
+      const autoHeight = JSON.parse(swiperElement.dataset.swiperAutoHeight || "false");
+      const slidesPerView = parseSlidesPerView(swiperElement.dataset.swiperSlidesPerView);
+      const spaceBetween = parseFloat(swiperElement.dataset.swiperSpace || 8);
+      const centeredSlides = JSON.parse(swiperElement.dataset.swiperCenteredSlides || "false");
+      const loop2 = JSON.parse(swiperElement.dataset.swiperLoop || "true");
+      const allowTouchMove = JSON.parse(swiperElement.dataset.swiperTouchMove || "true");
+      const autoplay = JSON.parse(swiperElement.dataset.swiperAutoplay || "true");
+      const autoplayDelay = parseFloat(swiperElement.dataset.swiperAutoplayDelay || 4e3);
+      const speed = parseFloat(swiperElement.dataset.swiperSpeed || 400);
+      const navigationPrefix = setNavigationPrefix(swiperId, swiperMode);
+      const prevEl = `${navigationPrefix}${dataNav}:not(.next)`;
+      const nextEl = `${navigationPrefix}${dataNav}.next`;
+      const autoplayOptions = setupAutoplay(autoplay, autoplayDelay);
+      const swiper = new Swiper(swiperElement, {
+        navigation: {
+          prevEl,
+          nextEl
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true
+        },
+        autoplay: autoplayOptions,
+        allowTouchMove,
+        centeredSlides,
+        effect: "slide",
+        speed,
+        autoHeight,
+        spaceBetween,
+        loop: loop2,
+        slidesPerView,
+        debugger: true
       });
-    }, {
-      threshold: 0.2
+      swiper.autoplay.stop();
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            swiper.autoplay.start();
+          } else {
+            swiper.autoplay.stop();
+          }
+        });
+      }, {
+        threshold: 0.2
+      });
+      observer.observe(swiperElement);
     });
-    observer.observe(swiperElement);
-  });
-  defaultSwipers = document.querySelectorAll(`[default-swiper-component]`);
-  defaultSwipers.forEach((swiperElement) => {
-    skipEmptySwiper(swiperElement);
-    swiperElement.classList.remove("initial-hide");
-    const swiperId = swiperElement.getAttribute("default-swiper-component");
-    const swiperMode = swiperElement.dataset.swiperMode;
-    const navigationPrefix = setNavigationPrefix(swiperId, swiperMode);
-    const swiper = new Swiper(swiperElement, {
-      navigation: {
-        prevEl: `${navigationPrefix}.swiper-button-static:not(.next)`,
-        nextEl: `${navigationPrefix}.swiper-button-static.next`
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true
-      },
-      allowTouchMove: true,
-      spaceBetween: 24,
-      speed: 400,
-      loop: true,
-      slidesPerView: "auto"
+  }
+  function initDefaultSwipers() {
+    const defaultSwipers = document.querySelectorAll(`[default-swiper-component]`);
+    defaultSwipers.forEach((swiperElement) => {
+      if (swiperEmpty(swiperElement))
+        return;
+      swiperElement.classList.remove("initial-hide");
+      const swiperId = swiperElement.getAttribute("default-swiper-component");
+      const swiperMode = swiperElement.dataset.swiperMode;
+      const navigationPrefix = setNavigationPrefix(swiperId, swiperMode);
+      const swiper = new Swiper(swiperElement, {
+        navigation: {
+          prevEl: `${navigationPrefix}.swiper-button-static:not(.next)`,
+          nextEl: `${navigationPrefix}.swiper-button-static.next`
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true
+        },
+        allowTouchMove: true,
+        spaceBetween: 24,
+        speed: 400,
+        loop: true,
+        slidesPerView: "auto"
+      });
     });
+  }
+  document.addEventListener("DOMContentLoaded", () => {
+    initWebflowSwipers();
+    initDefaultSwipers();
   });
 })();
--- src/js/swiper.js
+++ src/js/swiper.js
@@ -46,102 +46,177 @@ function setupAutoplay(enabled, delay = 4000) {
   }
 }
 
-// Remove all empty slides
-document.querySelectorAll(".w-slide:empty").forEach((e) => e.remove());
+function parseSwiperOptions(attributes) {
+  const settings = {};
+  attributes.forEach((attribute, index) => {
+    const name = attribute;
 
-const webflowSwipers = document.querySelectorAll(
-  '[swiper-component]:not([swiper-component="default"])'
-);
+  })
+  return settings;
+}
 
-webflowSwipers.forEach((swiperElement) => {
-  if (skipEmptySwiper(swiperElement)) return;
-  swiperElement.classList.remove("initial-hide");
+function initWebflowSwipers() {
+  // Remove all empty slides
+  document.querySelectorAll(".w-slide:empty").forEach((e) => e.remove());
 
-  const swiperId = swiperElement.getAttribute("swiper-component");
-  const swiperMode = swiperElement.dataset.swiperMode;
-  const navigationPrefix = setNavigationPrefix(swiperId, swiperMode);
-
-  const dataNav = (swiperElement.dataset.swiperNav || ".swiper-button").toString();
-  const prevEl = `${navigationPrefix}${dataNav}:not(.next)`;
-  const nextEl = `${navigationPrefix}${dataNav}.next`;
-  const autoHeight = JSON.parse(
-    swiperElement.dataset.swiperAutoHeight || "false"
+  const webflowSwipers = document.querySelectorAll(
+    '[swiper-component]:not([swiper-component="default"])'
   );
-  const slidesPerView = parseSlidesPerView(swiperElement.dataset.swiperSlidesPerView);
-  const spaceBetween = parseFloat(swiperElement.dataset.swiperSpace || 8);
-  const centeredSlides = JSON.parse(
-    swiperElement.dataset.swiperCenteredSlides || "false"
-  );
-  const loop = JSON.parse(swiperElement.dataset.swiperLoop || "true");
-  const allowTouchMove = JSON.parse(
-    swiperElement.dataset.swiperTouchMove || "true"
-  );
-  const autoplayBoolean = JSON.parse(swiperElement.dataset.swiperAutoplay || "true");
-  const autoplayDelay = parseFloat(swiperElement.dataset.swiperAutoplayDelay || 4000);
-  const autoplayOptions = setupAutoplay(autoplayBoolean, autoplayDelay);
-  const speed = parseFloat(swiperElement.dataset.swiperSpeed || 400);
 
-  const swiper = new Swiper(swiperElement, {
-    navigation: {
-      prevEl: prevEl,
-      nextEl: nextEl,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    autoplay: autoplayOptions,
-    allowTouchMove: allowTouchMove,
-    centeredSlides: centeredSlides,
-    effect: "slide",
-    speed: speed,
-    autoHeight: autoHeight,
-    spaceBetween: spaceBetween,
-    loop: loop,
-    slidesPerView: slidesPerView,
-    debugger: true,
+  webflowSwipers.forEach((swiperElement) => {
+    //if (skipEmptySwiper(swiperElement)) {
+    //  hideEmptySwiper(swiperElement)
+    //  return;
+    //}
+    swiperElement.classList.remove("initial-hide");
+
+    const attributes = [
+      {
+        name: "swiper-component",
+        type: "component"
+      },
+      {
+        name: "data-swiper-mode",
+        type: "text"
+      },
+      {
+        name: "data-swiper-nav",
+        type: "text",
+        default: ".swiper-button"
+      },
+      {
+        name: "data-swiper-auto-height",
+        type: "boolean",
+        default: false
+      },
+      {
+        name: "data-swiper-slides-per-view",
+        type: "floatOrAuto",
+      },
+      {
+        name: "data-swiper-space",
+        type: "float",
+        default: 8
+      },
+      {
+        name: "data-swiper-centered-slides",
+        type: "boolean",
+        default: false
+      },
+      {
+        name: "data-swiper-loop",
+        type: "boolean",
+        default: true
+      },
+      {
+        name: "data-swiper-touch-move",
+        type: "boolean",
+        default: true
+      },
+      {
+        name: "data-swiper-autoplay",
+        type: "boolean",
+        default: true
+      },
+      {
+        name: "data-swiper-autoplay-delay",
+        type: "float",
+        default: 4000
+      },
+      {
+        name: "data-swiper-speed",
+        type: "float",
+        default: 400
+      },
+    ]
+
+    const swiperId = swiperElement.getAttribute("swiper-component");
+    const swiperMode = swiperElement.dataset.swiperMode;
+    const dataNav = (swiperElement.dataset.swiperNav || ".swiper-button").toString();
+    const autoHeight = JSON.parse(swiperElement.dataset.swiperAutoHeight || "false");
+    const slidesPerView = parseSlidesPerView(swiperElement.dataset.swiperSlidesPerView);
+    const spaceBetween = parseFloat(swiperElement.dataset.swiperSpace || 8);
+    const centeredSlides = JSON.parse(swiperElement.dataset.swiperCenteredSlides || "false");
+    const loop = JSON.parse(swiperElement.dataset.swiperLoop || "true");
+    const allowTouchMove = JSON.parse(swiperElement.dataset.swiperTouchMove || "true");
+    const autoplay = JSON.parse(swiperElement.dataset.swiperAutoplay || "true");
+    const autoplayDelay = parseFloat(swiperElement.dataset.swiperAutoplayDelay || 4000);
+    const speed = parseFloat(swiperElement.dataset.swiperSpeed || 400);
+
+    const navigationPrefix = setNavigationPrefix(swiperId, swiperMode);
+    const prevEl = `${navigationPrefix}${dataNav}:not(.next)`;
+    const nextEl = `${navigationPrefix}${dataNav}.next`;
+    const autoplayOptions = setupAutoplay(autoplay, autoplayDelay);
+
+    const swiper = new Swiper(swiperElement, {
+      navigation: {
+        prevEl: prevEl,
+        nextEl: nextEl,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      autoplay: autoplayOptions,
+      allowTouchMove: allowTouchMove,
+      centeredSlides: centeredSlides,
+      effect: "slide",
+      speed: speed,
+      autoHeight: autoHeight,
+      spaceBetween: spaceBetween,
+      loop: loop,
+      slidesPerView: slidesPerView,
+      debugger: true,
+    });
+
+    swiper.autoplay.stop();
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          swiper.autoplay.start();
+        } else {
+          swiper.autoplay.stop();
+        }
+      })
+    }, {
+      threshold: 0.2
+    });
+
+    observer.observe(swiperElement);
   });
+}
 
-  swiper.autoplay.stop();
+function initDefaultSwipers() {
+  const defaultSwipers = document.querySelectorAll(`[default-swiper-component]`);
 
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        swiper.autoplay.start();
-      } else {
-        swiper.autoplay.stop();
-      }
-    })
-  }, {
-    threshold: 0.2
+  defaultSwipers.forEach((swiperElement) => {
+    if (swiperEmpty(swiperElement)) return;
+    swiperElement.classList.remove("initial-hide");
+
+    const swiperId = swiperElement.getAttribute("default-swiper-component");
+    const swiperMode = swiperElement.dataset.swiperMode;
+    const navigationPrefix = setNavigationPrefix(swiperId, swiperMode);
+
+    const swiper = new Swiper(swiperElement, {
+      navigation: {
+        prevEl: `${navigationPrefix}.swiper-button-static:not(.next)`,
+        nextEl: `${navigationPrefix}.swiper-button-static.next`,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      allowTouchMove: true,
+      spaceBetween: 24,
+      speed: 400,
+      loop: true,
+      slidesPerView: "auto",
+    });
   });
+}
 
-  observer.observe(swiperElement);
-});
-
-defaultSwipers = document.querySelectorAll(`[default-swiper-component]`);
-
-defaultSwipers.forEach((swiperElement) => {
-  skipEmptySwiper(swiperElement);
-  swiperElement.classList.remove("initial-hide");
-
-  const swiperId = swiperElement.getAttribute("default-swiper-component");
-  const swiperMode = swiperElement.dataset.swiperMode;
-  const navigationPrefix = setNavigationPrefix(swiperId, swiperMode);
-
-  const swiper = new Swiper(swiperElement, {
-    navigation: {
-      prevEl: `${navigationPrefix}.swiper-button-static:not(.next)`,
-      nextEl: `${navigationPrefix}.swiper-button-static.next`,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    allowTouchMove: true,
-    spaceBetween: 24,
-    speed: 400,
-    loop: true,
-    slidesPerView: "auto",
-  });
+document.addEventListener('DOMContentLoaded', () => {
+  initWebflowSwipers();
+  initDefaultSwipers();
 });
