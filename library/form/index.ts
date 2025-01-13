a94e612cdd8f336195930d105f95f75244c2713d
Lukas Graf <graflukas218@gmail.com>
2025-01-13 07:43:02 +0100

    add FormDecision and FormMessage to export library

--- library/form/index.ts
+++ library/form/index.ts
@@ -3,4 +3,6 @@ export * from './form';
 
 // Export FormField class and FieldData interface
 export * from './formfield';
-export * from './fieldgroup'
+export * from './fieldgroup';
+export * from './formmessage';
+export * from './formdecision'
