546ce42efddc8d7db50fde6850c1122f1dfe05d0
Lukas Graf <graflukas218@gmail.com>
2025-01-13 07:40:29 +0100

    fix indenting

--- library/form/fieldgroup.ts
+++ library/form/fieldgroup.ts
@@ -1,15 +1,15 @@
 import { FormField } from "@library/form/formfield";
 
 export class FieldGroup {
-        public fields: Map<string, FormField>;
+  public fields: Map<string, FormField>;
 
-        constructor(fields: Map<string, FormField> = new Map()) {
-                this.fields = fields;
-        }
+  constructor(fields: Map<string, FormField> = new Map()) {
+    this.fields = fields;
+  }
 
-        // Method to retrieve a field by its id
-        getField(fieldId: string): FormField | undefined {
-                return this.fields.get(fieldId);
-        }
+  // Method to retrieve a field by its id
+  getField(fieldId: string): FormField | undefined {
+    return this.fields.get(fieldId);
+  }
 }
 
