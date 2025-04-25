b11f9e4d61d951631ab6d59b280ed504e5d22ede
Lukas Graf <graflukas218@gmail.com>
2025-04-25 12:05:23 +0200

    add attribute types

--- library/attributeselector.ts
+++ library/attributeselector.ts
@@ -1,4 +1,17 @@
 type AttributeSelector = (name: string | null) => string;
+type AttributeType =
+  | 'startsWith'    // ^=
+  | 'endsWith'      // $=
+  | 'includes'      // *=
+  | 'whitespace'    // ~=
+  | 'hyphen'        // |=
+  | 'exact';        // =
+
+interface AttributeOptions<T> {
+  defaultType?: AttributeType;
+  defaultValue: T | null;
+  exclusions?: string[];
+}
 
 /**
  * Excludes a CSS selector from a CSS selector.
@@ -22,18 +35,51 @@ function exclude(selector: string, ...exclusions: string[]): string {
  * If no name is provided, it will return a selector with just the attribute name.
  * 
  * @template T - The type of the name that will be passed to the generated selector function (e.g., string).
- * @param  attrName - The name of the attribute that will be used in the selector.
- * @param [defaultValue=null] - An optional default value for the name, which is `null` by default.
- * @returns A function that generates the selector string based on the provided name.
+ * @param attrName - The name of the attribute that will be used in the selector.
+ * @param options - Options to configure selector generation.
+ * @returns A function that generates the selector string based on the provided name and match type.
  */
-const createAttribute = <T>(attrName: string, defaultValue: T | null = null, ...exclusions: string[]) => {
-  return (name: T | null = defaultValue): string => {
+const createAttribute = <T>(
+  attrName: string,
+  options: AttributeOptions<T> = {
+    defaultType: 'exact',
+    defaultValue: null,
+    exclusions: [],
+  },
+) => {
+  return (name: T | null = options.defaultValue, type: AttributeType = options.defaultType): string => {
     if (!name) {
-      return exclude(`[${attrName}]`, ...exclusions);
+      return exclude(`[${attrName}]`, ...options.exclusions);
     }
-    return exclude(`[${attrName}="${name}"]`, ...exclusions);
+
+    const value = String(name); // Ensure it's a string for selector use
+    let selector: string;
+
+    switch (type) {
+      case 'startsWith':
+        selector = `[${attrName}^="${value}"]`;
+        break;
+      case 'endsWith':
+        selector = `[${attrName}$="${value}"]`;
+        break;
+      case 'includes':
+        selector = `[${attrName}*="${value}"]`;
+        break;
+      case 'whitespace':
+        selector = `[${attrName}~="${value}"]`;
+        break;
+      case 'hyphen':
+        selector = `[${attrName}|="${value}"]`;
+        break;
+      case 'exact':
+      default:
+        selector = `[${attrName}="${value}"]`;
+        break;
+    }
+
+    return exclude(selector, ...(options.exclusions ?? []));
   };
 }
 
 export default createAttribute;
-export type { AttributeSelector };
+export type { AttributeSelector, AttributeType, AttributeOptions };
