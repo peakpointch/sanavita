c975f38536474776219f2dcec529623c2d632086
Lukas Graf <graflukas218@gmail.com>
2025-06-03 21:56:45 +0200

    feat: allow to specify linked fields through linkFields method

--- src/sanavita/ts/residentprospect.ts
+++ src/sanavita/ts/residentprospect.ts
@@ -9,6 +9,15 @@ export type GroupName =
   | "primaryRelative"
   | "secondaryRelative";
 
+type LinkedFieldsId =
+  | "heimatort"
+  | "phone"
+  | "email"
+  | "address"
+  | "doctor"
+  | "primaryRelative"
+  | "secondaryRelative";
+
 export function prospectMapToObject(prospects: Map<string, ResidentProspect>): any {
   // Convert a ResidentProspect's structure, which contains FieldGroups with fields as Maps
   const prospectsObj: any = {};
@@ -36,19 +45,36 @@ export class ResidentProspect {
   secondaryRelative: FieldGroup;
 
   key: string = `person-${crypto.randomUUID()}`;
+  linkedFields: Map<LinkedFieldsId | string, string[]>;
 
   constructor(
     personalData = new FieldGroup(),
     doctor = new FieldGroup(),
     health = new FieldGroup(),
     primaryRelative = new FieldGroup(),
-    secondaryRelative = new FieldGroup()
+    secondaryRelative = new FieldGroup(),
+    linkedFields = new Map<LinkedFieldsId | string, string[]>
   ) {
     this.personalData = personalData;
     this.doctor = doctor;
     this.health = health;
     this.primaryRelative = primaryRelative;
     this.secondaryRelative = secondaryRelative;
+    this.linkedFields = linkedFields;
+  }
+
+  public linkFields(groupId: LinkedFieldsId | string, fields: string): void {
+    if (!groupId) throw new Error(`ResidentProspect "${this.getFullName()}": The group id "${groupId}" for linking fields is not valid.`);
+
+    const inputIds = fields
+      ?.split(',')
+      .map(id => id.trim());
+
+    if (inputIds.length === 0 || inputIds.some(id => id === '')) {
+      throw new Error(`Please specify the ids of the fields you want to link. Ensure no ids are an empty string.`);
+    }
+
+    this.linkedFields.set(groupId, inputIds);
   }
 
   public validate(): boolean {
