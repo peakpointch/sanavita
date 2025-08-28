62a07885e7fa60831364a49996918729dcfedb92
Lukas Graf <graflukas218@gmail.com>
2025-08-28 20:32:35 +0200

    delete: unused dist files

--- dist/form.js
+++
@@ -1,3 +0,0 @@
-(() => {
-})();
-//# sourceMappingURL=form.js.map
--- dist/w-autoplay.js
+++
@@ -1,9 +0,0 @@
-(() => {
-  // src/ts/w-autoplay.ts
-  document.addEventListener("DOMContentLoaded", () => {
-    setTimeout(() => {
-      document.body.querySelectorAll(`[data-autoplay="true"] video[autoplay]`).forEach((video) => video.play());
-    }, 1e3);
-  });
-})();
-//# sourceMappingURL=w-autoplay.js.map
