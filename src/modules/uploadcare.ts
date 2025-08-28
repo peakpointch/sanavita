da723dc7fe3dd31829b5000b9ab69b0bb4680439
Lukas Graf <graflukas218@gmail.com>
2025-08-28 19:33:40 +0200

    refactor: uploadcare and wfvideo

--- src/modules/uploadcare.ts
+++ src/modules/uploadcare.ts
@@ -1,25 +1,80 @@
-// This script will handle multiple file uploads through uploadcare and submit them via webflow forms.
-const UUID_FIELD_ID = 'uploadcare-uuid';  // ID of your input field for the uuid(s)
-const URLCDN_FIELD_ID = 'uploadcare-file';  // ID of your input field for the url(s)
+import type { HTMLFormInput } from "peakflow";
 
-// Submit files
-const ctxProvider = document.querySelector('uc-upload-ctx-provider')
-ctxProvider.addEventListener('change', event => {
-  const files = event.detail.successEntries;
-  console.log("FILES:", files);  // Log files to the console
+type UCUploadStatus = "uploading" | "success" | "failed" | "idle";
+type UCEvent = CustomEvent<UCEventDetail>;
 
-  let uuidArray = files.map(file => {
-    return file.uuid;
+interface UCEventDetail {
+  progress: number;
+  errors: any[];
+  group: string | null;
+  totalCount: number;
+  failedCount: number;
+  successCount: number;
+  uploadingCount: number;
+  status: UCUploadStatus;
+  isSuccess: boolean;
+  isUploading: boolean;
+  isFailed: boolean;
+  allEntries: UCFile[];
+  successEntries: UCFile[];
+  failedEntries: UCFile[];
+  uploadingEntries: UCFile[];
+  idleEntries: UCFile[];
+}
+
+interface UCFile {
+  uuid: string | null;
+  internalId: string;
+  name: string;
+  size: number;
+  isImage: boolean;
+  mimeType: string;
+  file: Record<string, any>; // can be more specific if needed
+  externalUrl: string | null;
+  cdnUrlModifiers: any; // keep as any or type properly if known
+  cdnUrl: string | null;
+  fullPath: string | null;
+  uploadProgress: number;
+  fileInfo: any; // can type this more specifically if needed
+  metadata: any; // can type this more specifically if needed
+  isSuccess: boolean;
+  isUploading: boolean;
+  isFailed: boolean;
+  isRemoved: boolean;
+  errors: any[];
+  status: UCUploadStatus;
+  source: "local" | "remote";
+}
+
+export function initUploadcare(component: HTMLElement): void {
+  // This script will handle multiple file uploads through uploadcare and submit them via webflow forms.
+  const UUID_FIELD_ID = "uploadcare-uuid"; // ID of your input field for the uuid(s)
+  const URLCDN_FIELD_ID = "uploadcare-file"; // ID of your input field for the url(s)
+
+  // Submit files
+  const ctxProvider = component.querySelector("uc-upload-ctx-provider");
+  ctxProvider.addEventListener("change", (event: UCEvent) => {
+    const files = event.detail.successEntries;
+    console.log("EVENT:", event);
+    console.log("FILES:", files); // Log files to the console
+
+    let uuidArray = files.map((file) => {
+      return file.uuid;
+    });
+
+    let cdnUrlArray = files.map((file) => {
+      return file.cdnUrl;
+    });
+
+    const uuidField = component.querySelector<HTMLFormInput>(
+      `#${UUID_FIELD_ID}`,
+    );
+    uuidField.value = uuidArray.join(", ");
+    uuidField.dispatchEvent(new Event("change", { bubbles: true }));
+    const urlField = component.querySelector<HTMLFormInput>(
+      `#${URLCDN_FIELD_ID}`,
+    );
+    urlField.value = cdnUrlArray.join(", ");
+    urlField.dispatchEvent(new Event("change", { bubbles: true }));
   });
-
-  let cdnUrlArray = files.map(file => {
-    return file.cdnUrl;
-  });
-
-  const uuidField = document.getElementById(UUID_FIELD_ID)
-  uuidField.value = uuidArray.join(', ');
-  uuidField.dispatchEvent(new Event('change', { bubbles: true }));
-  const urlField = document.getElementById(URLCDN_FIELD_ID)
-  urlField.value = cdnUrlArray.join(', ');
-  urlField.dispatchEvent(new Event('change', { bubbles: true }));
-});
+}
--- src/modules/wfvideo.ts
+++ src/modules/wfvideo.ts
@@ -1,15 +1,16 @@
 // COMPONENT
-const VIDEO_COMPONENT_SELECTOR = '[data-player-component]';
+const VIDEO_COMPONENT_SELECTOR = "[data-player-component]";
 
 // ELEMENTS
-const VIDEO_SELECTOR = '[data-player-video]';
+const VIDEO_SELECTOR = "[data-player-video]";
 const CONTROLS_SELECTOR = '[data-player-element="controls"]';
 const PLAY_BUTTON_SELECTOR = '[data-player-button="play"]';
 const MUTE_BUTTON_SELECTOR = '[data-player-button="mute"]';
 
-const createVideoComponent = (component) => {
-  const videoElement = component.querySelector(VIDEO_SELECTOR);
-  const controls = component.querySelector(CONTROLS_SELECTOR);
+export function createVideoComponent(component: HTMLElement) {
+  const videoElement =
+    component.querySelector<HTMLVideoElement>(VIDEO_SELECTOR);
+  const controls = component.querySelector<HTMLElement>(CONTROLS_SELECTOR);
   const playButton = component.querySelector(PLAY_BUTTON_SELECTOR);
   const muteButton = component.querySelector(MUTE_BUTTON_SELECTOR);
 
@@ -19,34 +20,38 @@ const createVideoComponent = (component) => {
 
   // Set video properties based on attributes
   videoElement.autoplay = false;
-  videoElement.removeAttribute('autoplay');
+  videoElement.removeAttribute("autoplay");
   videoElement.loop = true;
-  videoElement.muted = isMuted;  // Initialize mute state based on data attribute
+  videoElement.muted = isMuted; // Initialize mute state based on data attribute
   if (isAutoplay) {
-    videoElement.play();  // Only autoplay if the attribute is true
+    videoElement.play(); // Only autoplay if the attribute is true
   }
 
   function showControls() {
-    controls ? controls.style.opacity = 1 : null;
+    controls ? (controls.style.opacity = "1") : null;
   }
   function hideControls() {
-    controls ? controls.style.opacity = 0 : null;
+    controls ? (controls.style.opacity = "0") : null;
   }
 
   // Function to toggle play/pause
   function togglePlay() {
-    const pauseState = playButton.querySelector('[data-player-button-state="pause"]');
-    const playState = playButton.querySelector('[data-player-button-state="play"]');
+    const pauseState = playButton.querySelector(
+      '[data-player-button-state="pause"]',
+    );
+    const playState = playButton.querySelector(
+      '[data-player-button-state="play"]',
+    );
 
     // Toggle video play/pause
     if (videoElement.paused) {
       videoElement.play();
-      playState.classList.add('hide');  // Hide play button
-      pauseState.classList.remove('hide');  // Show pause button
+      playState.classList.add("hide"); // Hide play button
+      pauseState.classList.remove("hide"); // Show pause button
     } else {
       videoElement.pause();
-      playState.classList.remove('hide');  // Show play button
-      pauseState.classList.add('hide');  // Hide pause button
+      playState.classList.remove("hide"); // Show play button
+      pauseState.classList.add("hide"); // Hide pause button
       showControls();
     }
   }
@@ -54,43 +59,49 @@ const createVideoComponent = (component) => {
   function toggleMute() {
     console.log("MUTE BUTTON PRESSED");
 
-    const muteState = muteButton.querySelector('[data-player-button-state="unmuted"]');
-    const unmuteState = muteButton.querySelector('[data-player-button-state="muted"]');
+    const muteState = muteButton.querySelector(
+      '[data-player-button-state="unmuted"]',
+    );
+    const unmuteState = muteButton.querySelector(
+      '[data-player-button-state="muted"]',
+    );
 
     // Toggle video mute/unmute
     if (videoElement.muted) {
       videoElement.muted = false;
-      muteState.classList.remove('hide');
-      unmuteState.classList.add('hide');
+      muteState.classList.remove("hide");
+      unmuteState.classList.add("hide");
     } else {
       videoElement.muted = true;
-      muteState.classList.add('hide');
-      unmuteState.classList.remove('hide');
+      muteState.classList.add("hide");
+      unmuteState.classList.remove("hide");
     }
   }
 
   // Show controls initially when video is paused or hasn't started yet
   showControls();
 
-  videoElement.addEventListener('pause', showControls);
+  videoElement.addEventListener("pause", showControls);
 
   // Add event listener for mouse events to show/hide controls
-  component.addEventListener('mouseenter', showControls);  // Show controls on hover
-  component.addEventListener('mouseleave', () => {
+  component.addEventListener("mouseenter", showControls); // Show controls on hover
+  component.addEventListener("mouseleave", () => {
     if (!videoElement.paused) {
-      hideControls();  // Hide controls only if the video is playing
+      hideControls(); // Hide controls only if the video is playing
     }
   });
 
   // Add event listeners
-  playButton.addEventListener('click', togglePlay);
-  muteButton?.addEventListener('click', toggleMute);
+  playButton.addEventListener("click", togglePlay);
+  muteButton?.addEventListener("click", toggleMute);
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  const components = document.querySelectorAll(VIDEO_COMPONENT_SELECTOR);
+export function initWfVideo() {
+  const components = document.querySelectorAll<HTMLElement>(
+    VIDEO_COMPONENT_SELECTOR,
+  );
 
   components.forEach((component) => {
     createVideoComponent(component);
   });
-});
+}
