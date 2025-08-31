2a2c52afae2233948bc1a02f8c997a284d1a27e2
Lukas Graf <graflukas218@gmail.com>
2025-08-31 12:11:02 +0200

    feat(aktuelles): update peakflow, use peakflow Script for finsweet code

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,5 @@
 import { WFRoute } from "@xatom/core";
-import peakflow, { Stylesheet } from "peakflow";
+import peakflow, { dateflow, Script, Stylesheet } from "peakflow";
 import { initBistroMenus } from "src/modules/menu";
 import { initWfVideo } from "src/modules/wfvideo";
 import { initVimePlayer } from "peakflow/video";
@@ -7,11 +7,12 @@ import { initApartmentRegistrationForm } from "src/modules/apartment-form";
 import { initCircleTabs } from "./jobs/circle-tabs.js";
 import { initSozjobsList } from "@/modules/sozjobs/list";
 import { initJobItemPage } from "@/modules/sozjobs/job";
+import { de } from "date-fns/locale";
 
 /**
  * WFRoute "/"
  */
-export const app = () => {
+export const app = async () => {
   new WFRoute("/").execute(() => {
     initWfVideo();
     peakflow.execute("inlinecms", "swiper", "dateflow");
@@ -54,6 +55,44 @@ export const app = () => {
 
   new WFRoute("/uber-uns").execute(() => {
     peakflow.execute("copyComponent");
+  });
+
+  new WFRoute("/aktuelles").execute(async () => {
+    await new Script({
+      src: "https://cdn.jsdelivr.net/npm/@finsweet/attributes@2/attributes.js",
+      type: "module",
+      async: true,
+      attributes: {
+        "fs-list": null,
+      },
+    }).load();
+
+    const listWrapperSelector = `[data-wf-collection="news"]`;
+    const listWrapper =
+      document.querySelector<HTMLElement>(listWrapperSelector);
+
+    dateflow(de, listWrapper);
+
+    const selector = `[fs-list-fuzzy]`;
+    const searchInputs = document.querySelectorAll<HTMLElement>(selector);
+    searchInputs.forEach((input) => {
+      input.addEventListener("input", () => {
+        dateflow(de, listWrapper);
+      });
+    });
+  });
+
+  new WFRoute("/dokumente").execute(() => {
+    new Script({
+      src: "https://cdn.jsdelivr.net/npm/@finsweet/attributes@2/attributes.js",
+      type: "module",
+      async: true,
+      attributes: {
+        "fs-list": null,
+        "fs-list-highlight": "true",
+        "fs-list-highlightclass": "is-hightlight",
+      },
+    }).load();
   });
 };
 
