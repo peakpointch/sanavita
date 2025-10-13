7df0e466ed0be0f0f6c485ef05053b7b9884a7d0
Lukas Manuel Graf <graflukas218@gmail.com>
2025-10-13 14:38:26 +0200

    refactor(form): refactor lindenpark-form setup, outsource initializing decisions

---
+++ src/modules/form/decisions.ts
@@ -0,0 +1,70 @@
+import { createAttribute, FormArray, FormDecision, MultiStepForm } from "peakflow";
+import { Resident } from "./resident";
+import { ContactPerson } from "./contact-person";
+import { Tenant } from "./tenant";
+
+const decisionSelector = createAttribute("data-decision-component");
+
+export function initializeFormDecisions(
+  form: MultiStepForm,
+  errorMessages: { [id: string]: { [key: string]: string } },
+  defaultMessages: { [id: string]: string } = {},
+): void {
+  form.formSteps.forEach((step, stepIndex) => {
+    const formDecisions = step.querySelectorAll<HTMLElement>(decisionSelector());
+
+    formDecisions.forEach((element) => {
+      const id = element.dataset.decisionComponent;
+      const decision = new FormDecision(element, { id });
+
+      // Set error messages for this FormDecision if available
+      if (id && errorMessages[id]) {
+        decision.setErrorMessages(errorMessages[id], defaultMessages[id]);
+      }
+
+      // Add the FormDecision as a custom component to the form
+      form.addCustomComponent({
+        stepIndex,
+        instance: decision,
+        validator: () => decision.validate(),
+      });
+    });
+  });
+}
+
+type PathId = string & ("show" | "hide");
+
+export function initializeArrayDecisions<T extends string = string>(
+  formArray: FormArray<Tenant | Resident | ContactPerson>,
+  errorMessages: { [id: string]: { [key: string]: string } },
+  defaultMessages: { [id: string]: string } = {},
+): Map<T, FormDecision<PathId>> {
+  const decisionElements = formArray.modalElement.querySelectorAll<HTMLElement>(decisionSelector());
+  const formDecisions: Map<T, FormDecision<PathId>> = new Map();
+
+  decisionElements.forEach((element, index) => {
+    const id = element.getAttribute(FormDecision.attr.component) || index.toString();
+    const decision = new FormDecision<PathId>(element, {
+      id,
+      clearPathOnChange: false,
+    });
+    formDecisions.set(decision.opts.id as T, decision);
+
+    const group = formArray.getClosestGroup(decision.component);
+    decision.onChange(() => {
+      formArray.validateModalGroup(group);
+      const valid = formArray.groups.every((group) => group.isValid === true);
+      formArray.splitButton.setAction(valid ? "save" : "draft");
+    });
+
+    formArray.onOpen(`decision-${id}`, () => decision.sync());
+    formArray.onClose(`decision-${id}`, () => decision.reset());
+
+    // Set error messages for this FormDecision if available
+    if (id && errorMessages[id]) {
+      decision.setErrorMessages(errorMessages[id], defaultMessages[id]);
+    }
+  });
+
+  return formDecisions;
+}
--- src/modules/lindenpark-form.ts
+++ src/modules/lindenpark-form.ts
@@ -1,108 +1,8 @@
-// Imports
-import {
-  FormArray,
-  MultiStepForm,
-  FormDecision,
-  FormProgressManager,
-  formElementSelector,
-} from "peakflow/form";
-import { createAttribute } from "peakflow/attributeselector";
+import { FormArray, MultiStepForm, FormProgressManager, formElementSelector } from "peakflow/form";
 import { Resident, SerializedResident } from "./form/resident";
 import { ContactPerson, SerializedContact } from "./form/contact-person";
+import { initializeFormDecisions, initializeArrayDecisions } from "./form/decisions";
 import { getAlertDialog } from "./form/alert-dialog";
-
-const decisionSelector = createAttribute("data-decision-component");
-
-function initializeFormDecisions(
-  form: MultiStepForm,
-  errorMessages: { [id: string]: { [key: string]: string } },
-  defaultMessages: { [id: string]: string } = {},
-): void {
-  form.formSteps.forEach((step, stepIndex) => {
-    const formDecisions =
-      step.querySelectorAll<HTMLElement>(decisionSelector());
-
-    formDecisions.forEach((element) => {
-      const id = element.dataset.decisionComponent;
-      const decision = new FormDecision(element, { id });
-
-      // Set error messages for this FormDecision if available
-      if (id && errorMessages[id]) {
-        decision.setErrorMessages(errorMessages[id], defaultMessages[id]);
-      }
-
-      // Add the FormDecision as a custom component to the form
-      form.addCustomComponent({
-        stepIndex,
-        instance: decision,
-        validator: () => decision.validate(),
-      });
-    });
-  });
-}
-
-type PathId = string & ("show" | "hide");
-
-function initializeProspectDecisions<T extends string = string>(
-  prospectArray: FormArray<Resident>,
-  errorMessages: { [id: string]: { [key: string]: string } },
-  defaultMessages: { [id: string]: string } = {},
-): Map<T, FormDecision<PathId>> {
-  const decisionElements =
-    prospectArray.modalElement.querySelectorAll<HTMLElement>(
-      decisionSelector(),
-    );
-  const formDecisions: Map<T, FormDecision<PathId>> = new Map();
-
-  decisionElements.forEach((element, index) => {
-    const id =
-      element.getAttribute(FormDecision.attr.component) || index.toString();
-    const decision = new FormDecision<PathId>(element, {
-      id,
-      clearPathOnChange: false,
-    });
-    formDecisions.set(decision.opts.id as T, decision);
-
-    const group = prospectArray.getClosestGroup(decision.component);
-    decision.onChange(() => {
-      prospectArray.validateModalGroup(group);
-      const valid = prospectArray.groups.every(
-        (group) => group.isValid === true,
-      );
-      prospectArray.splitButton.setAction(valid ? "save" : "draft");
-    });
-
-    prospectArray.onOpen(`decision-${id}`, () => decision.sync());
-    prospectArray.onClose(`decision-${id}`, () => decision.reset());
-
-    // Set error messages for this FormDecision if available
-    if (id && errorMessages[id]) {
-      decision.setErrorMessages(errorMessages[id], defaultMessages[id]);
-    }
-  });
-
-  return formDecisions;
-}
-
-function insertSearchParamValues(): void {
-  if (window.location.search) {
-    const params = new URLSearchParams(window.location.search);
-    const selectElement = document.querySelector(
-      "#wohnung",
-    ) as HTMLInputElement;
-
-    const wohnungValue = params.get("wohnung");
-    const option = selectElement.querySelector(
-      `option[value="${wohnungValue}"]`,
-    );
-    if (wohnungValue && option) {
-      // If you want to handle cases where the value doesn't exist
-      selectElement.value = wohnungValue;
-    } else {
-      console.warn(`No matching option for value: ${wohnungValue}`);
-    }
-  }
-}
 
 export function initRoomRegistrationForm(): void {
   const version = "1.0.0";
@@ -121,7 +21,7 @@ export function initRoomRegistrationForm(): void {
   progressManager.initForm(formId, version);
 
   const alertDialog = getAlertDialog();
-  const prospectArray = new FormArray<Resident>({
+  const ResidentArray = new FormArray<Resident>({
     id: "residents",
     formId: formId,
     container: formElement,
@@ -138,6 +38,9 @@ export function initRoomRegistrationForm(): void {
         sg: "die",
         pl: "die",
       },
+    },
+    messages: {
+      empty: "Bitte fügen Sie mindestens eine Person hinzu.",
     },
     dialogs: {
       delete: {
@@ -157,7 +60,8 @@ export function initRoomRegistrationForm(): void {
       },
     },
   });
-  const contactArray = new FormArray<ContactPerson>({
+
+  const ContactArray = new FormArray<ContactPerson>({
     id: "contacts",
     formId: formId,
     container: formElement,
@@ -193,7 +97,8 @@ export function initRoomRegistrationForm(): void {
       },
     },
   });
-  const FORM = new MultiStepForm(formElement, {
+
+  const LindenparkForm = new MultiStepForm(formElement, {
     id: formId,
     version: version,
     navigation: {
@@ -201,38 +106,33 @@ export function initRoomRegistrationForm(): void {
     },
     recaptcha: true,
     manager: progressManager,
-    excludeInputSelectors: [
-      '[data-decision-path="upload"]',
-      "[data-decision-component]",
-    ],
+    excludeInputSelectors: ['[data-decision-path="upload"]', "[data-decision-component]"],
     jsonFields: false,
   });
 
-  FORM.addCustomComponent({
+  LindenparkForm.addCustomComponent({
     stepIndex: 1,
-    instance: prospectArray,
-    validator: () => prospectArray.validate(),
+    instance: ResidentArray,
+    validator: () => ResidentArray.validate(),
     getData: () => {
       const data = {};
-      data[prospectArray.id] = JSON.stringify(prospectArray.serialize());
+      data[ResidentArray.id] = JSON.stringify(ResidentArray.serialize());
       return data;
     },
-  });
-  FORM.addCustomComponent({
-    stepIndex: 2,
-    instance: contactArray,
-    validator: () => contactArray.validate(),
-    getData: () => {
-      const data = {};
-      data[contactArray.id] = JSON.stringify(contactArray.serialize());
-      return data;
-    },
-  });
-  FORM.component.addEventListener("changeStep", () => {
-    if (prospectArray.modal.opened) prospectArray.closeModal();
   });
 
-  FORM.virtualFields.set("recipients", ({ customFields }) => {
+  LindenparkForm.addCustomComponent({
+    stepIndex: 2,
+    instance: ContactArray,
+    validator: () => ContactArray.validate(),
+    getData: () => {
+      const data = {};
+      data[ContactArray.id] = JSON.stringify(ContactArray.serialize());
+      return data;
+    },
+  });
+
+  LindenparkForm.virtualFields.set("recipients", ({ customFields }) => {
     const people: Array<SerializedResident | SerializedContact> = [
       ...JSON.parse(customFields.residents),
       ...JSON.parse(customFields.contacts),
@@ -242,7 +142,7 @@ export function initRoomRegistrationForm(): void {
     return JSON.stringify(emails);
   });
 
-  FORM.virtualFields.set("greetings", ({ customFields }) => {
+  LindenparkForm.virtualFields.set("greetings", ({ customFields }) => {
     const people: Array<SerializedResident | SerializedContact> = [
       ...JSON.parse(customFields.residents),
     ];
@@ -258,7 +158,7 @@ export function initRoomRegistrationForm(): void {
     return names;
   });
 
-  FORM.virtualFields.set("names", ({ fields, customFields }) => {
+  LindenparkForm.virtualFields.set("names", ({ fields, customFields }) => {
     const people: Array<SerializedResident | SerializedContact> = [
       ...JSON.parse(customFields.residents),
       ...JSON.parse(customFields.contacts),
@@ -286,60 +186,51 @@ export function initRoomRegistrationForm(): void {
     return JSON.stringify(names);
   });
 
-  const errorMessages = {
-    attachmentSubmission: {
-      upload: "Bitte laden Sie alle Beilagen hoch.",
-    },
-  };
-
-  const defaultMessages = {
-    attachmentSubmission: `Bitte laden Sie alle Beilagen hoch oder w├ñhlen Sie die Option "Beilagen per Post senden".`,
-  };
-
-  initializeProspectDecisions(prospectArray, errorMessages, defaultMessages);
-  initializeFormDecisions(FORM, errorMessages, defaultMessages);
-  insertSearchParamValues();
-
-  prospectArray.loadProgress();
-  contactArray.loadProgress();
-  prospectArray.registerSelects("(Ich)");
-  contactArray.registerSelects("(Kontaktperson)");
-
-  FORM.events.on("success", () => {
-    progressManager.clearForm(formId);
-    FORM.events.emit("save");
+  LindenparkForm.events.on("changeStep", () => {
+    if (ResidentArray.modal.opened) ResidentArray.closeModal();
   });
 
-  FORM.events.on("input", () => FORM.saveFields());
+  LindenparkForm.events.on("success", () => {
+    progressManager.clearForm(formId);
+    LindenparkForm.events.emit("save");
+  });
 
-  prospectArray.onSave("save-progress", (component) =>
-    FORM.saveComponentProgress(component),
+  LindenparkForm.events.on("input", () => LindenparkForm.saveFields());
+
+  ResidentArray.onSave("save-progress", (component) =>
+    LindenparkForm.saveComponentProgress(component),
   );
-  prospectArray.onSave("hide-add", () => {
-    const addButtonWrapper = prospectArray.select("add").parentElement;
-    if (prospectArray.items.size === prospectArray.options.limit) {
+  ResidentArray.onSave("hide-add", () => {
+    const addButtonWrapper = ResidentArray.select("add").parentElement;
+    if (ResidentArray.items.size === ResidentArray.options.limit) {
       addButtonWrapper.style.display = "none";
     } else {
       addButtonWrapper.style.removeProperty("display");
     }
   });
-  contactArray.onSave("save-progress", (component) =>
-    FORM.saveComponentProgress(component),
+  ContactArray.onSave("save-progress", (component) =>
+    LindenparkForm.saveComponentProgress(component),
   );
 
-  prospectArray.triggerOnSave();
-  contactArray.triggerOnSave();
+  ResidentArray.loadProgress();
+  ContactArray.loadProgress();
+  ResidentArray.registerSelects("(Ich)");
+  ContactArray.registerSelects("(Kontaktperson)");
+  ResidentArray.triggerOnSave();
+  ContactArray.triggerOnSave();
 
-  // Load progress here, otherwise recovering FormArray selects fails
-  FORM.loadProgress();
+  initializeFormDecisions(LindenparkForm, {});
+  initializeArrayDecisions(ResidentArray, {});
+
+  LindenparkForm.loadProgress();
 
   // @ts-ignore
-  window.MultiStepForm = FORM;
-  console.log("Form initialized:", FORM.initialized, FORM);
+  window.MultiStepForm = LindenparkForm;
+  console.log("Form initialized:", LindenparkForm.initialized, LindenparkForm);
 
-  // FORM.options.validation.validate = false;
-  // FORM.changeToStep(4);
-  // FORM.submit();
-  // await new Promise(resolve => setTimeout(resolve, 600));
-  // prospectArray.editProspect(prospectArray.getProspect(0));
+  // LindenparkForm.options.validation.validate = false;
+  // LindenparkForm.changeToStep(4);
+  // setTimeout(() => {
+  //   ResidentArray.editItem(ResidentArray.getItem(0))
+  // }, 600);
 }
