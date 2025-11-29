14ecc23a0bb300efa4c5d3f0f832699044d0d58c
Lukas Manuel Graf <graflukas218@gmail.com>
2025-11-29 11:48:52 +0100

    feat(components): add first react test component

--- src/components/main.dev.tsx
+++ src/components/main.dev.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Badge } from "./badge";
 
 // Import any component inside your repo:
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div style={{ padding: 40 }}>
       <h1>Components Playground</h1>
+      <Badge text="Hello Motherfuckers" variant="Light"></Badge>
     </div>
   );
 }
@@ -14,5 +16,5 @@ function App() {
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
