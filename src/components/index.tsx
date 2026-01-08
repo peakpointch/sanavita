6af87276ba91e19dda26310850d991cac15ed037
Lukas Manuel Graf <graflukas218@gmail.com>
2026-01-08 17:29:17 +0100

    feat(components/clock): add Clock component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -4,12 +4,14 @@ import "@/styles/globals.css";
 
 /* --- COMPONENT --- */
 import { WeatherWidget } from "./weather-widget/WeatherWidget";
+import { Clock } from "./clock/Clock";
 
 function App() {
   return (
-    <div className="min-h-screen p-10 flex flex-col items-center justify-center bg-brand-900 text-white">
+    <div className="min-h-screen p-10 flex flex-col gap-32 items-center justify-center bg-brand-900 text-white">
       {/* <h1>Components Playground</h1> */}
-      <WeatherWidget days={4} showMinMaxTemp={true} />
+      <Clock />
+      {/* <WeatherWidget days={4} showMinMaxTemp={true} /> */}
     </div>
   );
 }
