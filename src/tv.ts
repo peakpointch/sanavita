77642af8ef0f80d7dd8117ea45756728a3584e23
Lukas Manuel Graf <graflukas218@gmail.com>
2026-01-19 15:55:41 +0100

    feat(tv): auto update the tv's cms content

--- src/tv.ts
+++ src/tv.ts
@@ -1,24 +1,62 @@
 import { onReady, WFRoute } from "@xatom/core";
-import peakflow from "peakflow";
+import {
+  peakflow,
+  Slider,
+  dateflow,
+  mergeOptions,
+  inlineCms,
+  wf,
+} from "peakflow";
 import { initWfVideo } from "./modules/wfvideo";
+import { autoRefresh } from "./modules/auto-refresh";
+import { populateSwiper } from "./modules/utils";
+import { de } from "date-fns/locale/de";
+import { Autoplay, Manipulation } from "swiper/modules";
 // import { initAutoScroll } from "./modules/tv/auto-scroll";
+
+interface TVGlobal {
+  refresh?: () => void;
+}
+
+declare global {
+  interface Window {
+    tv: TVGlobal;
+  }
+}
 
 onReady(() => {
   const homeRoutes = ["/tv/home", "/tv/design"];
 
   for (const route of homeRoutes) {
     new WFRoute(route).execute(() => {
-      peakflow.execute("inlinecms", "swiper");
+      window.tv = window.tv || {};
+
+      peakflow.execute("inlinecms");
       peakflow.execute("dateflow");
+
+      Slider.initAll(document.body, {
+        modules: [Autoplay, Manipulation],
+      });
+
       initWfVideo();
+
       // initAutoScroll();
-      initReload();
+
+      autoRefresh({
+        delay: 60 * 60, // Refresh once every hour
+        beforeRefresh: ({ newDoc }) => {
+          console.log("Refreshing");
+
+          inlineCms({
+            origins: "[data-inlinecms-component]",
+            doc: newDoc,
+          });
+
+          dateflow(de, newDoc.body);
+
+          // populateSwiper({ name: "news", amount: 4, doc: newDoc });
+        },
+      });
     });
   }
 });
-
-function initReload(): void {
-  setInterval(() => {
-    window.location.reload();
-  }, 1 * 60 * 60 * 1000); // Update every hour
-}
