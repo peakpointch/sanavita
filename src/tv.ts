4e015c1c293d18ba9d2200a410b8d44501913545
Lukas Manuel Graf <graflukas218@gmail.com>
2026-01-22 15:37:35 +0100

    refactor(tv): reorganize

--- src/tv.ts
+++ src/tv.ts
@@ -1,22 +1,10 @@
 import { onReady, WFRoute } from "@xatom/core";
-import {
-  peakflow,
-  Slider,
-  dateflow,
-  mergeOptions,
-  inlineCms,
-  wf,
-} from "peakflow";
+import { peakflow, Slider, dateflow, inlineCms } from "peakflow";
 import { initWfVideo } from "./modules/wfvideo";
 import { autoRefresh } from "./modules/auto-refresh";
-import { populateSwiper } from "./modules/utils";
 import { de } from "date-fns/locale/de";
 import { Autoplay, Manipulation } from "swiper/modules";
 // import { initAutoScroll } from "./modules/tv/auto-scroll";
-
-interface TVGlobal {
-  refresh?: () => void;
-}
 
 declare global {
   interface Window {
@@ -24,39 +12,57 @@ declare global {
   }
 }
 
+interface TVGlobal {
+  refresh?: () => void;
+  refreshCore?: () => void;
+  failed: number;
+}
+
 onReady(() => {
   const homeRoutes = ["/tv/home", "/tv/design"];
 
   for (const route of homeRoutes) {
-    new WFRoute(route).execute(() => {
-      window.tv = window.tv || {};
-
-      peakflow.execute("inlinecms");
-      peakflow.execute("dateflow");
-
-      Slider.initAll(document.body, {
-        modules: [Autoplay, Manipulation],
-      });
-
-      initWfVideo();
-
-      // initAutoScroll();
-
-      autoRefresh({
-        delay: 60 * 60, // Refresh once every hour
-        beforeRefresh: ({ newDoc }) => {
-          console.log("Refreshing");
-
-          inlineCms({
-            origins: "[data-inlinecms-component]",
-            doc: newDoc,
-          });
-
-          dateflow(de, newDoc.body);
-
-          // populateSwiper({ name: "news", amount: 4, doc: newDoc });
-        },
-      });
-    });
+    new WFRoute(route).execute(initTV);
   }
 });
+
+function initTVGlobals(): void {
+  window.tv = window.tv || {
+    failed: 0,
+  };
+}
+
+function initTV(): void {
+  initTVGlobals();
+
+  peakflow.execute("inlinecms");
+  peakflow.execute("dateflow");
+
+  Slider.initAll(document.body, {
+    modules: [Autoplay, Manipulation],
+  });
+
+  initWfVideo();
+
+  // initAutoScroll();
+
+  autoRefresh({
+    delay: 60 * 60, // Refresh once every hour
+    // nodes: ({ mode }) => mode !== "code-component",
+    beforeRefresh: ({ newDoc }) => {
+      console.log("Refreshing");
+
+      inlineCms({
+        origins: "[data-inlinecms-component]",
+        doc: newDoc,
+      });
+
+      dateflow(de, newDoc.body);
+
+      // populateSwiper({ name: "news", amount: 4, doc: newDoc });
+    },
+    beforeNodeRefresh: ({ id, mode }) => {
+      console.log(`${mode}:${id}`);
+    },
+  });
+}
