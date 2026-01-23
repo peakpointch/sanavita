5bff35715521032ad8bffb34793afb565f798619
Lukas Manuel Graf <graflukas218@gmail.com>
2026-01-23 20:54:28 +0100

    fix(components): failed hmr in development (vitejs)
    - move utilities out of WeatherWidget.tsx to

--- src/components/weather-widget/WeatherWidget.tsx
+++ src/components/weather-widget/WeatherWidget.tsx
@@ -1,110 +1,17 @@
-import React, { JSX } from "react";
-import OpenWeatherMap from "openweathermap-ts";
+import React from "react";
 import type { WeatherDay, WeatherForecast } from "./types";
 import { getIconColor, weatherIconMap } from "./icons";
 import { de as omwDe } from "./i18n";
 import { de } from "date-fns/locale";
-import {
-  Droplet,
-  HelpCircle,
-  LucideIcon,
-  Thermometer,
-  Wind,
-} from "lucide-react";
+import { Droplet, Thermometer, Wind } from "lucide-react";
 import { format } from "date-fns";
 import { cn } from "@/lib/utils";
-import { openWeatherMapApiKey } from "@/secrets";
-import { currentWeather, forecast } from "./constants";
-
-const openWeather = new OpenWeatherMap({
-  apiKey: openWeatherMapApiKey,
-  units: "metric",
-});
-
-async function fetchCurrentWeather(fetch: boolean = true): Promise<WeatherDay> {
-  return new Promise(async (resolve) => {
-    if (!fetch) {
-      return resolve(currentWeather);
-    }
-
-    const data = await openWeather.getCurrentWeatherByZipcode(5210, "CH");
-    resolve(data as WeatherDay);
-  });
-}
-
-async function fetchForecast(fetch: boolean = true): Promise<WeatherForecast> {
-  return new Promise(async (resolve) => {
-    if (!fetch) {
-      return resolve(forecast);
-    }
-
-    const data = await openWeather.getThreeHourForecastByZipcode(5210, "CH");
-    resolve(data as WeatherForecast);
-  });
-}
-
-interface DayForecast {
-  date: Date;
-  temp: number;
-  minTemp: number;
-  maxTemp: number;
-  icon: {
-    node: LucideIcon;
-    color: string;
-  };
-}
-
-export function getDaysFromForecast(
-  forecast: WeatherForecast,
-  prod: boolean = true
-): DayForecast[] {
-  const dailyGroups = forecast.list.reduce<Record<string, DayForecast>>(
-    (acc, item) => {
-      // Parse date
-      const date = new Date(item.dt * 1000);
-      date.setHours(0, 0, 0, 0);
-      const dateStr = format(date, "yyyy-MM-dd");
-
-      // Skip today and past days
-      if (prod && date <= new Date()) return acc;
-
-      if (!acc || !acc[dateStr]) {
-        acc[dateStr] = {
-          date: date,
-          temp: item.main.temp,
-          minTemp: item.main.temp_min,
-          maxTemp: item.main.temp_max,
-          icon: {
-            node: weatherIconMap[item.weather[0].icon] || HelpCircle,
-            color: getIconColor(item.weather[0].icon),
-          },
-        };
-      } else {
-        // Skip forecasts from the same day
-        acc[dateStr].minTemp = Math.min(
-          acc[dateStr].minTemp,
-          item.main.temp_min
-        );
-        acc[dateStr].maxTemp = Math.max(
-          acc[dateStr].maxTemp,
-          item.main.temp_max
-        );
-      }
-
-      return acc;
-    },
-    {}
-  );
-
-  return Object.entries(dailyGroups).map<DayForecast>(([_, item]) => {
-    return {
-      ...item,
-      temp: Math.round(item.temp),
-      minTemp: Math.round(item.minTemp),
-      maxTemp: Math.round(item.maxTemp),
-    };
-  });
-}
+import {
+  type DayForecast,
+  fetchCurrentWeather,
+  fetchForecast,
+  getDaysFromForecast,
+} from "./utils";
 
 export interface WeatherWidgetProps {
   /** Controls the visibility of this component */
