3595e989bf1804406533ee6330cb9fa2e9ebbe52
Lukas Manuel Graf <graflukas218@gmail.com>
2026-03-30 21:59:30 +0200

    refactor(styles): restructure src/styles/ directory

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "@/styles/globals.css";
+import "@/styles/components/globals.css";
 
 /* --- COMPONENT --- */
 import { WeatherWidget } from "./weather-widget/WeatherWidget";
@@ -19,5 +19,5 @@ function App() {
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
