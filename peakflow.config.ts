b7f6b5999caca3e68f374317f675d5488b82f7f9
Lukas Manuel Graf <graflukas218@gmail.com>
2026-05-18 13:50:39 +0200

    temporary changes

---
+++ peakflow.config.ts
@@ -0,0 +1,100 @@
+import { defineConfig } from "peakflow/config";
+
+export default defineConfig({
+  repository: {
+    owner: "peakpointch",
+    name: "sanavita",
+  },
+  build: {
+    modules: [
+      "./src/app.ts",
+      "./src/admin.ts",
+      "./src/screen.ts",
+      "./src/tv.ts",
+      "./src/styles/app.css",
+    ],
+    outdir: "./dist",
+  },
+  server: {
+    webflowSubdomain: "sanavita-ag",
+    port: 3000,
+    livereload: true,
+    watchList: ["./src/", "../../peakflow/src/form/"],
+  },
+  environments: [
+    {
+      name: "website",
+      modules: ["app.js"],
+      version: "0.2.0-beta.4",
+      pages: ["index", "lindenpark", "wohnen-mit-service", "bistro/*", "dokumente"],
+    },
+    {
+      name: "forms",
+      modules: [
+        {
+          file: "app.js",
+          version: "0.2.0-beta.0",
+        },
+        "styles/app.css",
+      ],
+      version: "0.2.0-beta.4",
+      pages: ["screen/**/*"],
+    },
+    {
+      name: "admin",
+      modules: ["admin.js"],
+      version: "0.2.0-beta.4",
+      pages: ["screen/*"],
+    },
+    {
+      name: "screen",
+      modules: ["screen.js"],
+      version: "0.2.0-beta.4",
+      pages: ["screen/*"],
+    },
+    {
+      name: "tv",
+      modules: ["tv.js"],
+      version: "0.2.0-beta.4",
+      pages: ["tv/*"],
+    },
+  ],
+  // modules: [
+  //   {
+  //     name: "website-js",
+  //     file: "app.js",
+  //     version: "0.2.0-beta.4",
+  //     pages: ["index", "lindenpark", "wohnen-mit-service", "bistro/*", "dokumente"],
+  //   },
+  //   {
+  //     name: "website-css",
+  //     file: "styles/app.css",
+  //     version: "0.2.0-beta.4",
+  //     pages: ["index", "lindenpark", "wohnen-mit-service", "bistro/*", "dokumente"],
+  //   },
+  //   {
+  //     name: "forms",
+  //     file: "app.js",
+  //     version: "0.2.0-beta.4",
+  //     pages: ["screen/**/*"],
+  //   },
+  //   {
+  //     name: "admin",
+  //     file: "admin.js",
+  //     version: "0.2.0-beta.4",
+  //     pages: ["screen/*"],
+  //   },
+  //   {
+  //     name: "screen",
+  //     file: "screen.js",
+  //     version: "0.2.0-beta.4",
+  //     pages: ["screen/*"],
+  //   },
+  //   {
+  //     name: "tv",
+  //     file: "tv.js",
+  //     version: "0.2.0-beta.4",
+  //     pages: ["tv/*"],
+  //   },
+  // ],
+});
