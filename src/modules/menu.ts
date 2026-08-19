beacda03eaad8b2c194bfb55d01e3ea6d5c6e60b
Lukas Manuel Graf <graflukas218@gmail.com>
2026-08-19 17:58:09 +0200

    chore(modules/menu): removed unused function, moved to peakflow

--- src/modules/menu.ts
+++ src/modules/menu.ts
@@ -247,19 +247,6 @@ function renderMenu(
   });
 }
 
-async function fetchDocument(path: string): Promise<Document> {
-  const securePath = path.startsWith("/") ? path.slice(1) : path;
-  const url = `${window.location.protocol}//${window.location.host}/${securePath}`;
-
-  const res = await fetch(url);
-  const text = await res.text();
-
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(text, "text/html");
-
-  return doc;
-}
-
 export async function initBistroMenus(): Promise<void> {
   const root = await fetchOwnDocument("/cms/menu-data");
   const menuListItems = getMenuItems(document);
