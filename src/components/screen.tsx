3a1b93a2f275c11e964ee4fdd24d0033ad88f303
Lukas Manuel Graf <graflukas218@gmail.com>
2026-08-20 14:49:01 +0200

    fix(css/components): scale up tailwind spacing in scaled designs

--- src/components/screen.tsx
+++ src/components/screen.tsx
@@ -5,11 +5,11 @@ export interface MenuProps {}
 
 export function Screen({}: MenuProps) {
   return (
-    <div className="wf is-scaled scaled-container flex flex-col items-center justify-start bg-brand-900 text-base">
+    <div className="wf is-scaled scaled-container flex flex-col items-center justify-end bg-brand-900 text-base">
       {/* <MenuList /> */}
       <div className="w-[calc(944*var(--wf-px))]">
         {/* <MenuCardSkeleton scaled /> */}
-        <MenuList scaled seasonal={"Seasonal"} limit={1} />
+        <MenuList scaled collapsible={false} seasonal={"Seasonal"} limit={1} skip={3} />
       </div>
     </div>
   );
