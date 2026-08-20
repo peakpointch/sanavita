c75efabbc840dfdb7301656a2d0bf50691c031e3
Lukas Manuel Graf <graflukas218@gmail.com>
2026-08-20 15:00:17 +0200

    chore(components): enable tv dev environment

--- src/components/tv.tsx
+++ src/components/tv.tsx
@@ -5,7 +5,7 @@ export interface MenuProps {}
 
 export function TV({}: MenuProps) {
   return (
-    <div className="wf is-scaled scaled-container text-base bg-brand-900 flex flex-col items-center justify-center">
+    <div className="wf is-scaled scaled-container flex flex-col items-center justify-center bg-brand-900 text-base">
       {/* <Clock /> */}
       <WeatherWidget days={4} showMinMaxTemp={true} prod={false} />
     </div>
